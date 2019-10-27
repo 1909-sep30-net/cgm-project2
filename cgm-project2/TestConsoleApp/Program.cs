6ba8900feb63666dece549feb68e6f7529622764
Gregory Y Favrot <gpyofavv@gmail.com>
2019-10-26 21:58:50 -0500

    Add Test for Database access
    Successfully printed the id request from the db, "1".

---
+++ cgm-project2/TestConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using DatLib = Data.Library;
+using LogLib = Logic.Library;
+
+namespace TestConsoleApp
+{
+    /// <summary>
+    /// This Project is to be used purely for proto-testing functionality prior to Unit Tests and Runtime tests in Web.App
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get the dbContext
+            var optionsBuilder = new DbContextOptionsBuilder<DatLib.Entities.ecgbhozpContext>();
+            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("db")); //############### The Configuration.GetConnectionString("db") is mocked in the console app by making a class like 'Hidden' called Configuration
+            using var dbContext = new DatLib.Entities.ecgbhozpContext(optionsBuilder.Options);
+
+            //Make a Repo
+            var testRepo = new DatLib.Repo.TestRepo(dbContext);
+
+            //Use the Repo to get data
+            var test = testRepo.GetTest().FirstOrDefault();
+
+            //Write the data to the console
+            Console.WriteLine($"The test id is {test.TestId}");
+        }
+    }
+}
