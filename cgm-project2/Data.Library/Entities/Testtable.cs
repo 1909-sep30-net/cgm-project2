4d974f85d5dc16e4eef47d0b9dd0f739d5a25306
Gregory Y Favrot <gpyofavv@gmail.com>
2019-10-26 20:59:11 -0500

    Add Data.Library
    Used DB-First  for testing purposes only, All further code will be generated with Code first

---
+++ cgm-project2/Data.Library/Entities/Testtable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Library.Entities
+{
+    public partial class Testtable
+    {
+        public int Testid { get; set; }
+    }
+}
