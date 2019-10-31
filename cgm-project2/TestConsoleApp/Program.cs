67d16cf4eb2219053bcb03812c453f0a65c61f93
Mark Moore <mark.c.moore@hotmail.com>
2019-10-30 22:15:52 -0500

    comments out the whole Main method in TestConsoleApp.Program.cs because program wouldn't compile due to multiple entry points.

--- cgm-project2/TestConsoleApp/Program.cs
+++ cgm-project2/TestConsoleApp/Program.cs
@@ -11,14 +11,14 @@ namespace TestConsoleApp
     /// </summary>
     class Program
     {
-        static void Main(string[] args)
-        {
-            /**
-            //Get the dbContext
-            var optionsBuilder = new DbContextOptionsBuilder<DatLib.Entities.ecgbhozpContext>();
-            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("db")); //############### The Configuration.GetConnectionString("db") is mocked in the console app by making a class like 'Hidden' called Configuration
-            using var dbContext = new DatLib.Entities.ecgbhozpContext(optionsBuilder.Options);
-            **/
-        }
+        //static void Main(string[] args)
+        //{
+        //    /**
+        //    //Get the dbContext
+        //    var optionsBuilder = new DbContextOptionsBuilder<DatLib.Entities.ecgbhozpContext>();
+        //    optionsBuilder.UseNpgsql(Configuration.GetConnectionString("db")); //############### The Configuration.GetConnectionString("db") is mocked in the console app by making a class like 'Hidden' called Configuration
+        //    using var dbContext = new DatLib.Entities.ecgbhozpContext(optionsBuilder.Options);
+        //    **/
+        //}
     }
 }
