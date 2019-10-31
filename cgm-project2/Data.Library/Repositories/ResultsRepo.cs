a6d2b753f78a4e838b3f9ba0f21adb037cb10ef7
Colton Clary <coltonclary0806@yahoo.com>
2019-10-31 13:48:28 -0500

    Added Results Repo

---
+++ cgm-project2/Data.Library/Repositories/ResultsRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DatLib = Data.Library;
+using LogLib = Logic.Library;
+
+namespace Data.Library.Repositories
+{
+    public class ResultsRepo
+    {
+        private readonly Entities.ecgbhozpContext _dbContext;
+
+        public ResultsRepo(Entities.ecgbhozpContext dbContext) =>
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        public void AddResultToDb(LogLib.Models.Result result)
+        {
+            DatLib.Entities.Result newResult = Mapper.MapResult(result);
+            _dbContext.Add(newResult);
+        }
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
