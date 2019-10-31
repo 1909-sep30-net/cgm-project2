ec94e99d559fce0d68da256788e3c3c2d82b790e
Mark Moore <mark.c.moore@hotmail.com>
2019-10-31 12:36:22 -0500

    adds a few NuGet packages

---
+++ cgm-project2/TestConsoleApp/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsoleApp
+{
+    public static class Configuration
+    {
+        private static string connectionString = "Host=salt.db.elephantsql.com;Port=5432;Database=ecgbhozp;Username=ecgbhozp;Password=3YKSZmIYGnqvKceoPKMZzc0n1h5CU_g0;";
+        
+        public static string GetConnectionString(string mockString)
+        {
+            return connectionString;
+        }
+    }
+}
