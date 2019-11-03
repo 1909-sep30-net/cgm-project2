c992a1c049ad760f8e7caa6b19a155b13b21a7c2
Mark Moore <mark.c.moore@hotmail.com>
2019-11-03 00:22:24 -0500

    impliments the first functions for TakeAQuizRepository and ITakeAQuizRepository.

---
+++ cgm-project2/Data.Library/Repositories/TakeAQuizRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using DatLib = Data.Library;
+using LogLib = Logic.Library;
+using Logic.Library.Interfaces;
+
+
+namespace Data.Library.Repositories
+{
+    public class TakeAQuizRepository : ITakeAQuizRepository
+    {
+        private readonly Entities.ecgbhozpContext _dbContext;
+        public TakeAQuizRepository(Entities.ecgbhozpContext dbContext) =>
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+
+        public IEnumerable<LogLib.Models.Title> GetQuizByNameOrId(int Id = -1, string title = null)
+        {
+            IQueryable<Entities.Title> titles = _dbContext.Title.AsNoTracking();
+
+            if (Id != -1)
+            {
+                titles = titles.Where(i => i.TitleId == Id);
+            }
+            if (title != null)
+            {
+                titles = titles.Where(i => i.TitleString == title);
+            }
+
+            return titles.Select(Mapper.MapTitle);
+        }
+
+
+        /// <summary>
+        /// This saves the state of the DB
+        /// </summary>
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ cgm-project2/Logic.Library/Interfaces/ITakeAQuizRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//using DatLib = Data.Library;
+using LogLib = Logic.Library;
+
+namespace Logic.Library.Interfaces
+{
+    public interface ITakeAQuizRepository
+    {
+        public IEnumerable<Models.Title> GetQuizByNameOrId(int Id = -1, string title = null);
+
+        public void Save();
+
+    }
+}
