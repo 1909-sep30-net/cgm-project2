d6881b0d417c507f8a1f2a5a2b05f8b2a2f033d1
Gregory Y Favrot <gpyofavv@gmail.com>
2019-11-06 16:33:46 -0600

    Add get/id to TakeAQuiz Controller, repo method broken.
    Also Made minor change migration to db and added more initial data to db

--- cgm-project2/Data.Library/Repositories/TakeAQuizRepository.cs
+++ cgm-project2/Data.Library/Repositories/TakeAQuizRepository.cs
@@ -73,8 +73,8 @@ namespace Data.Library.Repositories
             quiz.title = quizTitle;
 
             //get all questions with the TitleId 
-            IQueryable<Entities.Question> quizQuestions = _dbContext.Question
-                .Where(i => i.TitleId == quiz.title.TitleId).AsNoTracking();
+            List<Entities.Question> quizQuestions = _dbContext.Question
+                .Where(i => i.TitleId == quiz.title.TitleId).AsNoTracking().ToList();
 
             //load mapped question obj into Quiz.questions List<Question> in foreach loop
             foreach (var item in quizQuestions)
