0b7cd7aabc07ee9d4c7ca65c60c006e1a8e56e08
Gregory Y Favrot <gpyofavv@gmail.com>
2019-11-06 21:45:46 -0600

    Add 5 tests for CreateQuizController

--- cgm-project2/XUnit.Test/ControllerTests/CreateQuizControllerTests.cs
+++ cgm-project2/XUnit.Test/ControllerTests/CreateQuizControllerTests.cs
@@ -17,6 +17,104 @@ namespace XUnit.Test.ControllerTests
     public class CreateQuizControllerTests
     {
 
+        [Fact]
+        public void PostCategorySucceeds()
+        {
+            //arrange
+            var mockRepo = new Mock<LogLib.Interfaces.ICreateQuizRepository>();
+            mockRepo.Setup(r => r.CreateCategory(It.IsAny<LogLibMod.Category>()));
 
+            var mockGetRepo = new Mock<LogLib.Interfaces.IGetDataRepository>();
+            mockGetRepo.Setup(r => r.TitleExists(It.IsAny<int>())).Returns(true);
+
+            var controller = new CreateQuizController(mockRepo.Object, mockGetRepo.Object);
+
+            //act assert
+            var statusCode = Assert.IsType<StatusCodeResult>(controller.PostCategory(new Rest.Api.Models.CategoryModel { }));
+            Assert.Equal(202, statusCode.StatusCode);
+        }
+
+        [Fact]
+        public void PostQuestionSucceeds()
+        {
+            //arrange
+            var mockRepo = new Mock<LogLib.Interfaces.ICreateQuizRepository>();
+            mockRepo.Setup(r => r.CreateQuestion(It.IsAny<LogLibMod.Question>()));
+
+            var mockGetRepo = new Mock<LogLib.Interfaces.IGetDataRepository>();
+            mockGetRepo.Setup(r => r.TitleExists(It.IsAny<int>())).Returns(true);
+
+            var controller = new CreateQuizController(mockRepo.Object, mockGetRepo.Object);
+
+            //act assert
+            var statusCode = Assert.IsType<StatusCodeResult>(controller.PostQuestion(new Rest.Api.Models.QuestionModel { }));
+            Assert.Equal(202, statusCode.StatusCode);
+        }
+
+        [Fact]
+        public void PostAnswerSucceeds()
+        {
+            //arrange
+            var mockRepo = new Mock<LogLib.Interfaces.ICreateQuizRepository>();
+            mockRepo.Setup(r => r.CreateAnswer(It.IsAny<LogLibMod.Answer>()));
+
+            var mockGetRepo = new Mock<LogLib.Interfaces.IGetDataRepository>();
+            mockGetRepo.Setup(r => r.QuestionExists(It.IsAny<int>())).Returns(true);
+            mockGetRepo.Setup(r => r.CategoryExists(It.IsAny<int>())).Returns(true);
+
+            var controller = new CreateQuizController(mockRepo.Object, mockGetRepo.Object);
+
+            //act assert
+            var statusCode = Assert.IsType<StatusCodeResult>(controller.PostAnswer(new Rest.Api.Models.AnswerModel { }));
+            Assert.Equal(202, statusCode.StatusCode);
+        }
+
+        [Fact]
+        public void PutRanksPassesCorrectly()
+        {
+            //arrange
+            var mockRepo = new Mock<LogLib.Interfaces.ICreateQuizRepository>();
+            mockRepo.Setup(r => r.SetCategoryRank(It.IsAny<int>(), It.IsAny<int>()));
+
+            var mockGetRepo = new Mock<LogLib.Interfaces.IGetDataRepository>();
+
+            var controller = new CreateQuizController(mockRepo.Object, mockGetRepo.Object);
+
+            var categories = new Rest.Api.Models.CategoryModel[1]
+            {
+                new Rest.Api.Models.CategoryModel
+                {
+                    Rank = 1
+                }
+            };
+
+            //act assert
+            var statusCode = Assert.IsType<StatusCodeResult>(controller.PutCategoryRanks(categories));
+            Assert.Equal(204, statusCode.StatusCode);
+        }
+
+        [Fact]
+        public void PutRanksFailsCorrectly()
+        {
+            //arrange
+            var mockRepo = new Mock<LogLib.Interfaces.ICreateQuizRepository>();
+            mockRepo.Setup(r => r.SetCategoryRank(It.IsAny<int>(), It.IsAny<int>()));
+
+            var mockGetRepo = new Mock<LogLib.Interfaces.IGetDataRepository>();
+
+            var controller = new CreateQuizController(mockRepo.Object, mockGetRepo.Object);
+
+            var categories = new Rest.Api.Models.CategoryModel[1]
+            {
+                new Rest.Api.Models.CategoryModel
+                {
+                    Rank = 7
+                }
+            };
+
+            //act assert
+            var statusCode = Assert.IsType<StatusCodeResult>(controller.PutCategoryRanks(categories));
+            Assert.Equal(406, statusCode.StatusCode);
+        }
     }
 }
