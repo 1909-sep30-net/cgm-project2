f937392b99a21e6558a6746a9e913a205c208671
Gregory Y Favrot <gpyofavv@gmail.com>
2019-11-07 15:46:22 -0600

    Add Angular App

---
+++ AngularApp/src/app/user-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
+import User from './models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUsers(): Promise<User[]> {
+    let url = `${environment.restApiBaseUrl}/api/User`;
+    return this.httpClient.get<User[]>(url).toPromise();
+
+  }
+}
