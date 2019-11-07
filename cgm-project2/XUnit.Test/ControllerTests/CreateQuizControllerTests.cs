70a24ce7f4173e78cd7fe77f14ee77a6b9681294
Colton Clary <coltonclary0806@yahoo.com>
2019-11-06 20:58:10 -0600

    Added Create Quiz Controller Tests Class

---
+++ cgm-project2/XUnit.Test/ControllerTests/CreateQuizControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Xunit;
+using LogLib = Logic.Library;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Linq;
+using Moq;
+using Data.Library.Repositories;
+using LogLibMod = Logic.Library.Models;
+using System.Collections;
+using System.Collections.Generic;
+using Rest.Api.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XUnit.Test.ControllerTests
+{
+    public class CreateQuizControllerTests
+    {
+
+
+    }
+}
